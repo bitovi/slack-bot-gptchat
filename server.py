6bf68a85f1f8c5a33b684489b71ffc59b13223aa
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2023-02-09 14:03:42 -0500

    fix slack credential mapping

--- server.py
+++ server.py
@@ -4,10 +4,16 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack import WebClient
 from slack_bolt import App
 
+# TODO: rename .env.example
+# OPENAI_API_KEY="sk-......."
+# SLACK_BOT_APP_TOKEN="xapp-......."
+# SLACK_API_KEY="xoxb-......." (SLACK_APP_TOKEN)
+slack_app_token = os.environ['SLACK_BOT_APP_TOKEN']
+slack_bot_token = os.environ['SLACK_API_KEY']
 
 # Event API & Web API
-app = App(token=os.environ['SLACK_BOT_APP_TOKEN']) 
-client = WebClient(os.environ['SLACK_BOT_APP_TOKEN'])
+app = App(token=slack_bot_token) 
+client = WebClient(slack_bot_token)
 
 # This gets activated when the bot is tagged in a channel    
 @app.event("app_mention")
@@ -40,4 +46,4 @@ def handle_message_events(body, logger):
                                        text=f"Here you go: \n{response}")
 
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ['SLACK_API_KEY']).start()
+    SocketModeHandler(app, slack_app_token).start()
