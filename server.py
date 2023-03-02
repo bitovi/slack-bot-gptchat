d3d02c34c497226b921d2bb9b74ec9dbaa9cef0e
LeoDiazL <116020815+LeoDiazL@users.noreply.github.com>
2023-03-02 16:24:19 -0300

    Add missing response. Support new API model. (#8)
    * Adding gpt-3 engine if and missing response
    * Fixing server.py if statement
    * Update docker-compose.yaml
    * Adding missing settings
    * Moving int conversion

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ slack_app_token = os.environ['SLACK_BOT_APP_TOKEN']
 slack_bot_token = os.environ['SLACK_API_KEY']
 # Optional Environment variables
 openai_engine = os.environ.get('OPENAI_ENGINE', 'gpt-3.5-turbo')
-openai_max_tokens = os.environ.get('OPENAI_MAX_TOKENS', '1024')
+openai_max_tokens = int(os.environ.get('OPENAI_MAX_TOKENS', '1024'))
 openai_ack_msg = os.environ.get('OPENAI_ACK_MSG', "Hello from your bot! :robot_face: \nThanks for your request, I'm on it!")
 openai_reply_msg = os.environ.get('OPENAI_REPLY_MSG', "Here you go: \n")
 
@@ -37,19 +37,32 @@ def handle_message_events(body, logger):
     
     # Check ChatGPT
     openai.api_key = openai_api_key
-    response = openai.Completion.create(
-        engine=openai_engine,
-        prompt=prompt,
-        max_tokens=int(openai_max_tokens),
-        n=1,
-        stop=None,
-        temperature=0.5).choices[0].text
+
+    if openai_engine.startswith('gpt-3.'):
+        response = openai.ChatCompletion.create(
+            model=openai_engine,
+            max_tokens=openai_max_tokens,
+            stop=None,
+            temperature=0.5,
+            messages=[
+                {"role": "user", "content": f"{prompt}"},
+            ]
+        ).choices[0].message.content
+
+    else:
+        response = openai.Completion.create(
+            engine=openai_engine,
+            prompt=prompt,
+            max_tokens=openai_max_tokens,
+            n=1,
+            stop=None,
+            temperature=0.5).choices[0].text
     
     
     # Reply to thread 
     response = client.chat_postMessage(channel=body["event"]["channel"], 
                                        thread_ts=body["event"]["event_ts"],
-                                       text=f"{openai_reply_msg}")
+                                       text=f"{openai_reply_msg}{response}")
 
 if __name__ == "__main__":
     SocketModeHandler(app, slack_app_token).start()
